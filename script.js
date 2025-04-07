841c38b5ac624ee68c1c2371e688353a92302ac8
Hrithik875 <160313400+Hrithik875@users.noreply.github.com>
2025-04-07 19:16:43 +0530

    added logo and made the navbar sticky

--- script.js
+++ script.js
@@ -90,4 +90,14 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 10) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
   
